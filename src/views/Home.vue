2cb56191e5042935250650eb0cf53aed00dfb2df
Mikolai Fedotov <starcrafters@ya.ru>
2022-01-30 15:10:09 +0700

    Add Shop Info block

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <ShopInfo />
     <TheFooter />
   </div>
 </template>
 
 <script lang="ts" setup>
 import TheFooter from "@/components/TheFooter/index.vue";
+import ShopInfo from "@/components/ShopInfo/ShopInfo.vue";
 </script>
